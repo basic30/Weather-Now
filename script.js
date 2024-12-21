f2fb4a3aa6fd4f4e628d047739c7e37cca2a159a
Snahasish Dey <82492280+basic30@users.noreply.github.com>
2024-12-22 00:36:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -77,79 +77,84 @@ async function getWeatherData(city) {
     }, []).slice(0, 5);
 
     return {
-        location: `${geoData[0].name}, ${geoData[0].country}`,
-        currentTemp: Math.round(currentWeather.main.temp),
-        condition: currentWeather.weather[0].main,
-        highTemp: Math.round(currentWeather.main.temp_max),
-        lowTemp: Math.round(currentWeather.main.temp_min),
-        hourlyForecast,
-        dailyForecast,
-        precipitation: `${currentWeather.clouds?.all || 0}%`,
-        humidity: `${currentWeather.main.humidity}%`,
-        wind: `${(currentWeather.wind.speed * 3.6).toFixed(1)} km/h`
-    };
+    location: `${geoData[0].name}, ${geoData[0].country}`,
+    currentTemp: Math.round(currentWeather.main.temp),
+    condition: currentWeather.weather[0].main,
+    conditionIcon: currentWeather.weather[0].icon, // Add this line
+    highTemp: Math.round(currentWeather.main.temp_max),
+    lowTemp: Math.round(currentWeather.main.temp_min),
+    hourlyForecast,
+    dailyForecast,
+    precipitation: `${currentWeather.clouds?.all || 0}%`,
+    humidity: `${currentWeather.main.humidity}%`,
+    wind: `${(currentWeather.wind.speed * 3.6).toFixed(1)} km/h`
+};
 }
 
 function displayWeatherData(weatherData) {
     const weatherHTML = `
-        <div class="weather-header">
-            <div>${weatherData.location}</div>
-            <div>Current Weather</div>
+    const weatherHTML = `
+    <div class="weather-header">
+        <div>${weatherData.location}</div>
+        <div>Current Weather</div>
+    </div>
+    <div class="weather-temp">${weatherData.currentTemp}°C</div>
+    <div class="weather-condition">
+        <img src="https://openweathermap.org/img/wn/${weatherData.conditionIcon}@2x.png" alt="${weatherData.condition}" style="width: 50px;">
+        ${weatherData.condition}
+    </div>
+    <div>H: ${weatherData.highTemp}°C | L: ${weatherData.lowTemp}°C</div>
+    
+    <div class="weather-section">
+        <h3>Hourly Forecast</h3>
+        <div class="hourly-forecast">
+            ${weatherData.hourlyForecast.map(hour => `
+                <div class="hourly-item">
+                    <img src="https://openweathermap.org/img/wn/${hour.icon}@2x.png" alt="${hour.temp}°C">
+                    <div>${hour.time}</div>
+                    <div>${hour.temp}°C</div>
+                </div>
+            `).join('')}
         </div>
-        <div class="weather-temp">${weatherData.currentTemp}°C</div>
-        <div class="weather-condition">${weatherData.condition}</div>
-        <div>H: ${weatherData.highTemp}°C | L: ${weatherData.lowTemp}°C</div>
-        
-        <div class="weather-section">
-            <h3>Hourly Forecast</h3>
-            <div class="hourly-forecast">
-                ${weatherData.hourlyForecast.map(hour => `
-                    <div class="hourly-item">
-                        <img src="https://openweathermap.org/img/wn/${hour.icon}@2x.png" alt="${hour.temp}°C">
-                        <div>${hour.time}</div>
-                        <div>${hour.temp}°C</div>
-                    </div>
-                `).join('')}
-            </div>
+    </div>
+    
+    <div class="weather-section">
+        <h3>5-Day Forecast</h3>
+        <div class="daily-forecast">
+            ${weatherData.dailyForecast.map(day => `
+                <div class="daily-item">
+                    <img src="https://openweathermap.org/img/wn/${day.icon}@2x.png" alt="icon">
+                    <div>${day.day}</div>
+                    <div>${day.highTemp}°C / ${day.lowTemp}°C</div>
+                </div>
+            `).join('')}
         </div>
-        
-        <div class="weather-section">
-            <h3>5-Day Forecast</h3>
-            <div class="daily-forecast">
-                ${weatherData.dailyForecast.map(day => `
-                    <div class="daily-item">
-                        <img src="https://openweathermap.org/img/wn/${day.icon}@2x.png" alt="icon">
-                        <div>${day.day}</div>
-                        <div>${day.highTemp}°C / ${day.lowTemp}°C</div>
-                    </div>
-                `).join('')}
-            </div>
+    </div>
+    
+    <div class="weather-details">
+        <div class="detail-box">
+            <h4>Precipitation</h4>
+            <p>
+                <img src="https://openweathermap.org/img/wn/09d@2x.png" alt="precipitation-icon" style="width: 30px;">
+                ${weatherData.precipitation}
+            </p>
         </div>
-        
-        <div class="weather-details">
-            <div class="detail-box">
-                <h4>Precipitation</h4>
-                <p>
-                    <img src="https://openweathermap.org/img/wn/09d@2x.png" alt="precipitation-icon" style="width: 30px;">
-                    ${weatherData.precipitation}
-                </p>
-            </div>
-            <div class="detail-box">
-                <h4>Humidity</h4>
-                <p>
-                    <img src="https://openweathermap.org/img/wn/50d@2x.png" alt="humidity-icon" style="width: 30px;">
-                    ${weatherData.humidity}
-                </p>
-            </div>
-            <div class="detail-box">
-                <h4>Wind</h4>
-                <p>
-                    <img src="https://openweathermap.org/img/wn/03d@2x.png" alt="wind-icon" style="width: 30px;">
-                    ${weatherData.wind}
-                </p>
-            </div>
+        <div class="detail-box">
+            <h4>Humidity</h4>
+            <p>
+                <img src="https://openweathermap.org/img/wn/50d@2x.png" alt="humidity-icon" style="width: 30px;">
+                ${weatherData.humidity}
+            </p>
         </div>
-    `;
+        <div class="detail-box">
+            <h4>Wind</h4>
+            <p>
+                <img src="https://openweathermap.org/img/wn/03d@2x.png" alt="wind-icon" style="width: 30px;">
+                ${weatherData.wind}
+            </p>
+        </div>
+    </div>
+`;
 
     // Insert the generated HTML into the weather display container
     weatherDisplay.innerHTML = weatherHTML;
